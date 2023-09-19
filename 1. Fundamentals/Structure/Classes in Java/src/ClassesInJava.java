1a58abfca1a7e22115280cfa14b581f09bcbbf9e
valko <kaarlocaballeronillukka@gmail.com>
2023-09-19 11:15:41 +0100

    First commit

--- 1. Fundamentals/Structure/Classes in Java/src/ClassesInJava.java
+++ 1. Fundamentals/Structure/Classes in Java/src/ClassesInJava.java
@@ -5,6 +5,6 @@ public class ClassesInJava
    public static void main(String[] args)
    {
       //This is a comment, ignored by Java.
-      //Place statements here.
+      //Place statements here
    }
 }
