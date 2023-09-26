718494a1864659835074d3f3869d438e16284692
valko <kaarlocaballeronillukka@gmail.com>
2023-09-26 10:49:12 +0100

    Fundamentals java for beginners

--- 1. Fundamentals/Operators/1.6 Boolean Calculator/src/BooleanCalculator.java
+++ 1. Fundamentals/Operators/1.6 Boolean Calculator/src/BooleanCalculator.java
@@ -4,5 +4,9 @@ public class BooleanCalculator
    {
       int a = 3, b = -4, c = 1;
       boolean t = true, f = false;
+      System.out.println(a >= b);
+      System.out.println(-b / a == c);
+      System.out.println((t || !f) == (2 * c <= c));
+      System.out.println((!t == f) != (b + c / a > a - b * c));
    }
 }
