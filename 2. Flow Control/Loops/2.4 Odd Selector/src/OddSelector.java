9e22c68e58da36d6593f6b2b93e7fffed1df7fd7
valko <kaarlocaballeronillukka@gmail.com>
2023-09-26 12:27:24 +0100

    Flow control - java for beginners

--- 2. Flow Control/Loops/2.4 Odd Selector/src/OddSelector.java
+++ 2. Flow Control/Loops/2.4 Odd Selector/src/OddSelector.java
@@ -5,9 +5,12 @@ public class OddSelector
       System.out.println("These are the odd numbers from 1 to 100.");
       System.out.println("Ready?");
       //for values from 1 to 100
-      for (int value = 1; value <= 100; value++)
+      /*for (int value = 1; value <= 100; value++)
       {
          System.out.println(value); //print them
+      }*/
+	   for (int value = 2; value <= 100; value += 2) {
+		   System.out.println(value);
       }
       System.out.println("That was fast!");
    }
