9531cb57e4648b5831198f2f692c039c5c4a99ca
valko <kaarlocaballeronillukka@gmail.com>
2023-09-30 17:03:07 +0100

    Objects and methods - java for beginners

--- 3. Objects and Methods/Methods/3.5 Make it A Combo/src/Meal.java
+++ 3. Objects and Methods/Methods/3.5 Make it A Combo/src/Meal.java
@@ -1,19 +1,23 @@
-public class Meal
-{
+public class Meal {
     //Instance attributes//
     private String name;
     private double cost;
 
     //Class constructor//
-    public Meal(String food, double price)
-    {
+    public Meal(String food, double price) {
         name = food;
         cost = price;
     }
 
     //Instance methods//
-   /* Complete the method makeItACombo */}
+    public void makeItACombo() {
+        cost += 3;
+        name += " with fries and soda";
+    }
 
-   /* Complete the method getBill */}
+    public String getBill() {
+        return name + ", $" + cost;
+    }
 
 }
+
